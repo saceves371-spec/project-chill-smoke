0b9ae19a99c237ff998f4db1c8ad48f02fdaf19f
Sergio Aceves <saceves371@gmail.com>
2026-02-01 05:12:08 +0000

    puedes usar esta imagen https://res.cloudinary.com/dogre008n/image/uploa

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
