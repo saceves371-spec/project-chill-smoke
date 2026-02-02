717fa5f1444c68b8d0542df99503ea7c7581f9c7
Sergio Aceves <saceves371@gmail.com>
2026-02-02 20:02:52 +0000

    y si intentas utilizando esta url para ponerla como fondo https://wallpa

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'wallpapers.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
